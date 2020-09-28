ba136cf9b557b1d98f4d748b48bb5c5563e50a91
vincentlacour <vincentlacour@gmail.com>
2020-09-28 14:31:11 -0400

    added example file

---
+++ example1.py
@@ -0,0 +1,24 @@
+
+
+
+def func1 (arg1, arg2):
+    """
+    This function takes 2 arguments, sets the first to equal the second, the returns the new first argument.  Just a useless example.
+
+    :param arg1: Some Value
+    :param ar2: Another Value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+
+
+def func2():
+    """
+    Function with no arguments returns None
+
+    :return: None
+    """
+    return None
